6b4d60b3479a4a5b72f6c11bc0e07b5538251eb2
dilute111 <loshock666@gmail.com>
2026-03-19 13:27:48 +0500

    moved http requests into constants

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404,
+}
+
 app.use(express.json())
 
 const db = {
@@ -35,36 +44,44 @@ app.get('/courses', (req, res) => {
 app.get('/courses/:id', (req, res) => {
     let courseQuery = db.courses.find(c => c.id === +req.params.id);
     if (!courseQuery) {
-        res.sendStatus(404)
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
         return
     }
     res.json(courseQuery)
 })
 
 app.post('/courses', (req, res) => {
+    if (!req.query.title) {
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+        return
+    }
     let newCourse = {
         id: +(new Date()),
         title: req.body.title
     };
     db.courses.push(newCourse)
-    res.status(201).json(newCourse)
+    res.status(HTTP_STATUSES.CREATED_201).json(newCourse)
 })
 
 app.delete('/courses/:id', (req, res) => {
     db.courses = db.courses.filter(c => c.id !== +req.params.id);
 
-    res.sendStatus(204)
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
 
 
 app.put('/courses/:id', (req, res) => {
+    if (!req.query.title) {
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+        return
+    }
     let courseQuery = db.courses.find(c => c.id === +req.params.id);
     if (!courseQuery) {
-        res.sendStatus(404)
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
         return
     }
     courseQuery.title = req.body.title
-    res.sendStatus(204)
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
 
 app.post('/users', (req, res) => {
